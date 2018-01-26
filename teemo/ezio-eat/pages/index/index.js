66062817caac9a27210689f44fa4c3ff3ebe72ae
haotian <haotian.li@ipiaoniu.com>
2018-01-26 20:02:52 +0800

    褐言生活指南-添加随机次数

--- teemo/ezio-eat/pages/index/index.js
+++ teemo/ezio-eat/pages/index/index.js
@@ -4,6 +4,10 @@ const app = getApp()
 
 Page({
   data: {
+    no_times: false,
+    frequency: 7,
+    pre_frequency: 1,
+    hidden: false,
     latitude: 1.00,
     longitude: 2.00,
     sw: "开始",
@@ -15,7 +19,7 @@ Page({
     list: ["馄饨", "拉面", "烩面", "热干面", "高贵沙县", "黄焖鸡米饭", "喝粥", "有家川菜", "花椒鱼"],
     shops: [],
     foods: [],
-    food:""
+    food: ""
   },
   //事件处理函数
   bindViewTap: function () {
@@ -63,29 +67,67 @@ Page({
     })
   },
   getEat: function (e) {
-    if (!this.data.run) {
+
+    if (!this.data.run && this.data.frequency > 0 && this.data.pre_frequency > 0) {
+
+      var n = this.data.frequency - 1;
+      var pre_n = this.data.frequency;
       this.setData({
+        pre_frequency: pre_n,
+        frequency: n,
         run: true,
         sw: "停止",
         foodImgUrl: ""
       })
+      if (n == 0) {
+        this.setData({
+          no_times: true
+        })
+      }
     } else {
+
       var path = this.data.food.image_path
-      var p1 = path.substring(0,1)
+      var p1 = path.substring(0, 1)
       var p2 = path.substring(1, 3)
       var p3 = path.substring(3, path.length)
-      path = p1+"/"+p2+"/"+p3
-      var url = "http://fuss10.elemecdn.com/" + path +".jpeg?imageMogr2/thumbnail/100x100/format/webp/quality/85"
+      path = p1 + "/" + p2 + "/" + p3
+      var url = "http://fuss10.elemecdn.com/" + path + ".jpeg?imageMogr2/thumbnail/100x100/format/webp/quality/85"
       console.log("url-->" + url)
       this.setData({
         run: false,
         sw: "不行，换一个",
         motto: "吃什么? 吃这个！",
-        foodImgUrl: url
       })
+
+      //一坨神奇的判断
+      if (this.data.pre_frequency == 0 && this.data.frequency <= 0) {
+        this.setData({
+          pre_frequency: -1,
+        })
+      }
+      if (this.data.pre_frequency == 1 && this.data.frequency <= 0) {
+        this.setData({
+          pre_frequency: 0,
+        })
+      }
+      if (this.data.pre_frequency >= 0 && this.data.frequency >= 0){
+        console.log("frequency-->" + this.data.frequency)
+        this.setData({
+          foodImgUrl: url
+        })
+      }
     }
-    //getFood(this)
-    getElmFood(this)
+
+    if (this.data.frequency <= 0 && this.data.pre_frequency < 0) {
+      wx.showToast({
+        title: '这么作，别吃了，等死吧，告辞~',
+        icon: "none",
+        duration: 2000
+      })
+    } else {
+      getElmFood(this)
+    }
+
   }
 
 })
@@ -106,7 +148,7 @@ function getLocation(that) {
 
 function getElmShops(that) {
   wx.request({
-    url: 'https://www.ele.me/restapi/shopping/restaurants?limit=10&latitude=' + that.data.latitude + '&longitude=' + that.data.longitude +"&restaurant_category_ids%5B%5D=-100&sign=1516949807400&terminal=web",
+    url: 'https://www.ele.me/restapi/shopping/restaurants?limit=10&latitude=' + that.data.latitude + '&longitude=' + that.data.longitude + "&restaurant_category_ids%5B%5D=-100&sign=1516949807400&terminal=web",
     data: {},
     method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
     // header: {}, // 设置请求的 header
@@ -128,10 +170,9 @@ function getElmShops(that) {
 }
 
 function getElmFoods(that, shops) {
-  var lastIndex = shops.length;
+  var index = 1;
   for (var i = 1; i < shops.length; i++) {
     var shop = shops[i];
-    var cIndex = i;
     wx.request({
       url: 'https://www.ele.me/restapi/shopping/v2/menu?restaurant_id=' + shop.id,
       data: {},
@@ -139,11 +180,10 @@ function getElmFoods(that, shops) {
       // header: {}, // 设置请求的 header
       success: function (res) {
         // success
-        that.setData({
-          foods: that.data.foods.concat(res.data[0].foods),
-        });
-        if (i == shops.length) {
-          console.log(that.data.foods);
+        if (res.data[0] != null) {
+          that.setData({
+            foods: that.data.foods.concat(res.data[0].foods),
+          });
         }
       },
       fail: function (res) {
@@ -152,6 +192,12 @@ function getElmFoods(that, shops) {
       },
       complete: function (res) {
         // complete
+        index++;
+        console.log("index" + index);
+        if (index == shops.length) {
+          console.log("========");
+          shuffle(that)
+        }
       }
     })
   }
@@ -160,6 +206,7 @@ function getElmFoods(that, shops) {
 
 
 function getElmFood(that) {
+
   console.log("getElmFood")
   var size = that.data.foods.length;
   if (that.data.run) {
@@ -175,6 +222,8 @@ function getElmFood(that) {
       getElmFood(that)
     }, 100)
   }
+
+
 }
 
 
@@ -189,5 +238,7 @@ function shuffle(that) {
   }
   that.setData({
     foods: a,
+    hidden: true
   });
+  console.log(that.data.foods);
 }
